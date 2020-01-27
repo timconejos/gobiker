24f2cc4e65c59238bd42052ab1e03c0baefd82ae
jammeejam <jkolila98@gmail.com>
2020-01-27 17:25:11 +0800

    Initial Frontend Commit

---
+++ app/src/main/java/ph/com/team/gobiker/SuccessVerification.java
@@ -0,0 +1,14 @@
+package ph.com.team.gobiker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_verification);
+    }
+}
