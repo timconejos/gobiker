7b7d8e7415bf33b3b40be01a78e5925e995a6bae
Tim Conejos <tbsc.09@gmail.com>
2020-03-04 12:46:24 +0800

    Timbriant loginprofile (#3)
    * integrated login
    
    * integrated drawer

--- app/src/main/java/ph/com/team/gobiker/data/LoginDataSource.java
+++ app/src/main/java/ph/com/team/gobiker/data/LoginDataSource.java
@@ -1,6 +1,17 @@
 package ph.com.team.gobiker.data;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import ph.com.team.gobiker.data.model.LoggedInUser;
+import ph.com.team.gobiker.login;
 
 import java.io.IOException;
 
@@ -9,15 +20,25 @@ import java.io.IOException;
  */
 public class LoginDataSource {
 
+    private FirebaseAuth mAuth;
+    private static boolean loginSuccess;
     public Result<LoggedInUser> login(String username, String password) {
 
         try {
             // TODO: handle loggedInUser authentication
+            String uid = firebaseAuthenticate(username, password);
             LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
                             "Jane Doe");
-            return new Result.Success<>(fakeUser);
+
+            if(loginSuccess){
+                return new Result.Success<>(new LoggedInUser(uid, "Sample"));
+            }
+            else{
+                return new Result.Error(new IOException("Invalid Username or password"));
+            }
+
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
@@ -25,5 +46,21 @@ public class LoginDataSource {
 
     public void logout() {
         // TODO: revoke authentication
+    }
+
+    private String firebaseAuthenticate(String username, String password){
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    loginSuccess = true;
+                }
+                else{
+                    loginSuccess = false;
+                }
+            }
+        });
+        return FirebaseAuth.getInstance().getCurrentUser().getUid().isEmpty() || FirebaseAuth.getInstance().getCurrentUser().getUid() == null ? FirebaseAuth.getInstance().getCurrentUser().getUid() : "";
     }
 }
