6690d02f7f8bd5037de564ba07c39e2b9602f66b
timbriant <tbsc.09@gmail.com>
2020-10-05 21:43:43 +0800

    fixed gmail login

--- app/src/main/java/ph/com/team/gobiker/ui/login/MainLoginActivity.java
+++ app/src/main/java/ph/com/team/gobiker/ui/login/MainLoginActivity.java
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -28,11 +29,18 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -58,12 +66,14 @@ import ph.com.team.gobiker.ui.map.PermissionUtils;
 public class MainLoginActivity extends AppCompatActivity {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private LoginViewModel loginViewModel;
-    private Button noAccBtn, forgotPassBtn, loginButton;
+    private Button noAccBtn, forgotPassBtn, loginButton, googleButton;
     private EditText UserEmail, UserPassword;
     private ProgressDialog loadingBar;
     private FirebaseAuth mAuth;
     private Boolean emailAddressChecker;
     private DatabaseReference UsersRef;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -72,10 +82,20 @@ public class MainLoginActivity extends AppCompatActivity {
         /*loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);*/
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
         UserEmail = findViewById(R.id.username);
         UserPassword = findViewById(R.id.login_password);
         loginButton = findViewById(R.id.continueLogin);
+        googleButton = findViewById(R.id.gLogin);
         noAccBtn = findViewById(R.id.login_no_account);
         forgotPassBtn = findViewById(R.id.login_forgot_pass);
         loadingBar = new ProgressDialog(this);
@@ -101,6 +121,13 @@ public class MainLoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AllowingUserToLogin();
+            }
+        });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gLogin();
             }
         });
 
@@ -181,6 +208,11 @@ public class MainLoginActivity extends AppCompatActivity {
                         passwordEditText.getText().toString());
             }
         });*/
+    }
+
+    private void gLogin() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     private void askPermission() {
@@ -300,5 +332,40 @@ public class MainLoginActivity extends AppCompatActivity {
 
         UsersRef.child(mAuth.getCurrentUser().getUid()).child("userState")
                 .updateChildren(currentStateMap);
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("gsignin", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("gsignin", "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("gsignin", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("gsignin", "Google sign in failed", e);
+            }
+        }
     }
 }
